9e7046d31eb3a0ee118358143b26926841cab748
KingRonnin <116754298+KingRonnin@users.noreply.github.com>
2023-11-12 20:17:52 -0700

    Update meal-ideas.js

--- app/week7/meal-ideas.js
+++ app/week7/meal-ideas.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 async function fetchMealIdeas(ingredient) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
     const data = await response.json();
-    return data.meals;
+    return data.meals || [];
 }
 
 export default function Page({ ingredient }) {
